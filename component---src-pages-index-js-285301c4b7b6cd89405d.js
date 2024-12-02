22639f9f77c3855d64083e3c66fd559cb8eea7f5
Malik <mudassirnaveed303@gmail.com>
2024-12-03 01:13:54 +0500

    Updates

---
+++ component---src-pages-index-js-285301c4b7b6cd89405d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[678],{6558:function(e,t,a){a.r(t);var l=a(7294);t.default=e=>{let{data:t}=e;return l.createElement("main",null," ",l.createElement("h1",null,t.site.siteMetadata.title),l.createElement("p",null,t.site.siteMetadata.description),l.createElement("p",null,t.site.siteMetadata.author))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-285301c4b7b6cd89405d.js.map
